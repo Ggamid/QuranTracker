d8736ec77f8c762939ccbf4242ad9f496e4cf069
Гамид Халидов <khalidov-gamid@mail.ru>
2024-11-10 14:51:53 +0300

    Initial Commit

---
+++ QuranTracker/QuranTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranTrackerApp.swift
+//  QuranTracker
+//
+//  Created by Gamıd Khalıdov on 10.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuranTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
