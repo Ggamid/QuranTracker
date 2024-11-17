b7b947aaa4f620bb8558ba35143fb4445868a231
Гамид Халидов <khalidov-gamid@mail.ru>
2024-11-17 13:03:03 +0300

    QuranReadingSession Модель данных для сохранения 1 сессии прочтения Корана

---
+++ QuranTracker/Model/DataModels/QuranReadingSession.swift
@@ -0,0 +1,33 @@
+//
+//  QuranReadingSession.swift
+//  QuranTracker
+//
+//  Created by Gamıd Khalıdov on 17.11.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class QuranReadingSession {
+    let startPage: Int
+    let endPage: Int
+    let sessionDate: Date
+    
+    var pageAmount: Int {
+        endPage - startPage
+    }
+
+    init(startPage: Int, endPage: Int, sessionDate: Date) {
+        self.startPage = startPage
+        self.endPage = endPage
+        self.sessionDate = sessionDate
+    }
+    
+    func getStringDate() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-yy"
+        
+        return formatter.string(from: self.sessionDate)
+    }
+}
