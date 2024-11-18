8083eff4a3cb95fe8d7ffdd3f42587d333f47c85
Гамид Халидов <khalidov-gamid@mail.ru>
2024-11-18 21:27:21 +0300

    getSurahName Написал метод который возвращает название первой суры на странице

--- QuranTracker/Views/WriteProgress/WriteProgress-ViewModel.swift
+++ QuranTracker/Views/WriteProgress/WriteProgress-ViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftData
+import SwiftUI
 
 extension WriteProgressView {
     @Observable
@@ -16,23 +17,42 @@ extension WriteProgressView {
         var startPage: Int = 0
         var endPage: Int = 0
         var presentAlert = false
+        var currentPage: Int?
+        var currentSurah: String?
 
         func saveReadingSession(modelContext: ModelContext) -> Bool {
             if checkNumbers() {
-                DispatchQueue.global().async {
-                    let readingSession = QuranReadingSession(
-                        startPage: self.startPage,
-                        endPage: self.endPage,
-                        sessionDate: self.sessionDate
-                    )
-                    modelContext.insert(readingSession)
-                }
+
+                let readingSession = QuranReadingSession(
+                    startPage: self.startPage,
+                    endPage: self.endPage,
+                    sessionDate: self.sessionDate
+                )
+
+                modelContext.insert(readingSession)
+
                 startPage = 0
                 endPage = 0
                 return true
             }
             presentAlert = true
             return false
+        }
+
+        @MainActor
+        func getSurahName(quranReadingSession: QuranReadingSession) async {
+            currentPage = quranReadingSession.endPage
+            do {
+                guard let currentPage else { return }
+                let quranPage = try await QuranData.fetchData(about: currentPage)
+                let surah = quranPage.surahs.sorted { Int($0.key)! < Int($1.key)! } // сортируем массив по номеру сур
+                // берем из массива первый элемент. там и будет содержаться название первой суры на странице Корана
+                currentSurah = surah.first?.value.englishName
+                return
+            } catch {
+                print("something went wrong")
+                return
+            }
         }
 
         func checkNumbers() -> Bool {
