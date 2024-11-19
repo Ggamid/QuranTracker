944285dce5b89e15c6ab4398660d942931394399
Гамид Халидов <khalidov-gamid@mail.ru>
2024-11-19 14:36:40 +0300

    сделал отобржаение прогресса ввиде прогресс бара

--- QuranTracker/Views/StatView.swift
+++ QuranTracker/Views/StatView.swift
@@ -6,11 +6,18 @@
 //
 
 import SwiftUI
+import SwiftData
+import SwiftfulUI
 
 struct StatView: View {
+
+    @Query(sort: \QuranReadingSession.sessionDate) var readingSessions: [QuranReadingSession]
+
     var body: some View {
         NavigationStack {
             VStack {
+                progressInPages
+
                 Image(.quranTrackerLogo)
                     .resizable()
                     .scaledToFit()
@@ -33,4 +40,44 @@ struct StatView: View {
 
 #Preview {
     StatView()
+}
+
+extension StatView {
+    @ViewBuilder
+    private var progressInPages: some View {
+        if !readingSessions.isEmpty {
+            VStack {
+                Text("Прогресс в страницах")
+                    .font(.title2)
+                    .fontWeight(.bold)
+
+                HStack {
+                    Text("0")
+                    Spacer()
+                    Text("604")
+                }
+                .font(.title2)
+                .frame(maxWidth: .infinity)
+
+                CustomProgressBar(
+                    selection: Double(readingSessions.last?.endPage ?? 0),
+                    range: 0...605,
+                    backgroundColor: .black,
+                    foregroundColor: .orangeQT,
+                    cornerRadius: 10,
+                    height: 8
+                )
+            }
+            .padding()
+            .background(.yellowQT)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .padding()
+        } else {
+            Text("Запишите свой первый прогресс чтобы увидеть статистику чтения!")
+                .font(.title3)
+                .padding()
+                .multilineTextAlignment(.center)
+                .fontWeight(.bold)
+        }
+    }
 }
